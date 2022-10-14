2973216a19eb800f93fa780e1321f8f3581dadb2
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2022-10-14 18:41:33 -0300

    Adicionado seed Fipe

---
+++ app/Database/Seeds/FipeSeeder.php
@@ -0,0 +1,268 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class FipeSeeder extends Seeder
+{
+    public function run()
+    {
+        $seeders = array(
+            [
+                'intervaloInicio' => 0,
+                'intervaloFim' => 500000,
+                'total' => 6323
+            ],
+            [
+                'intervaloInicio' => 500000,
+                'intervaloFim' => 800000,
+                'total' => 7066
+            ],
+            [
+                'intervaloInicio' => 800000,
+                'intervaloFim' => 1100000,
+                'total' => 7973
+            ],
+            [
+                'intervaloInicio' => 1100000,
+                'intervaloFim' => 1400000,
+                'total' => 8881
+            ],
+            [
+                'intervaloInicio' => 1400000,
+                'intervaloFim' => 1700000,
+                'total' => 9772
+            ],
+            [
+                'intervaloInicio' => 1700000,
+                'intervaloFim' => 2000000,
+                'total' => 10679
+            ],
+            [
+                'intervaloInicio' => 2000000,
+                'intervaloFim' => 2300000,
+                'total' => 11570
+            ],
+            [
+                'intervaloInicio' => 2300000,
+                'intervaloFim' => 2600000,
+                'total' => 12478
+            ],
+            [
+                'intervaloInicio' => 2600000,
+                'intervaloFim' => 2900000,
+                'total' => 13369
+            ],
+            [
+                'intervaloInicio' => 2900000,
+                'intervaloFim' => 3200000,
+                'total' => 14276
+            ],
+            [
+                'intervaloInicio' => 3200000,
+                'intervaloFim' => 3500000,
+                'total' => 15167
+            ],
+            [
+                'intervaloInicio' => 3500000,
+                'intervaloFim' => 3800000,
+                'total' => 16075
+            ],
+            [
+                'intervaloInicio' => 3800000,
+                'intervaloFim' => 4100000,
+                'total' => 16966
+            ],
+            [
+                'intervaloInicio' => 4100000,
+                'intervaloFim' => 4400000,
+                'total' => 17873
+            ],
+            [
+                'intervaloInicio' => 4400000,
+                'intervaloFim' => 4700000,
+                'total' => 18781
+            ],
+            [
+                'intervaloInicio' => 4700000,
+                'intervaloFim' => 5000000,
+                'total' => 19688
+            ],
+            [
+                'intervaloInicio' => 5000000,
+                'intervaloFim' => 5300000,
+                'total' => 20596
+            ],
+            [
+                'intervaloInicio' => 5300000,
+                'intervaloFim' => 5600000,
+                'total' => 21503
+            ],
+            [
+                'intervaloInicio' => 5600000,
+                'intervaloFim' => 5900000,
+                'total' => 22411
+            ],
+            [
+                'intervaloInicio' => 5900000,
+                'intervaloFim' => 6200000,
+                'total' => 23318
+            ],
+            [
+                'intervaloInicio' => 6200000,
+                'intervaloFim' => 6500000,
+                'total' => 24226
+            ],
+            [
+                'intervaloInicio' => 6500000,
+                'intervaloFim' => 6800000,
+                'total' => 25133
+            ],
+            [
+                'intervaloInicio' => 6800000,
+                'intervaloFim' => 7100000,
+                'total' => 26041
+            ],
+            [
+                'intervaloInicio' => 7100000,
+                'intervaloFim' => 7400000,
+                'total' => 26948
+            ],
+            [
+                'intervaloInicio' => 7400000,
+                'intervaloFim' => 7700000,
+                'total' => 27856
+            ],
+            [
+                'intervaloInicio' => 7700000,
+                'intervaloFim' => 8000000,
+                'total' => 28763
+            ],
+            [
+                'intervaloInicio' => 8000000,
+                'intervaloFim' => 8300000,
+                'total' => 29671
+            ],
+            [
+                'intervaloInicio' => 8300000,
+                'intervaloFim' => 8600000,
+                'total' => 30578
+            ],
+            [
+                'intervaloInicio' => 8600000,
+                'intervaloFim' => 8900000,
+                'total' => 31486
+            ],
+            [
+                'intervaloInicio' => 8900000,
+                'intervaloFim' => 9200000,
+                'total' => 32393
+            ],
+            [
+                'intervaloInicio' => 9200000,
+                'intervaloFim' => 9500000,
+                'total' => 33301
+            ],
+            [
+                'intervaloInicio' => 9500000,
+                'intervaloFim' => 9800000,
+                'total' => 34208
+            ],
+            [
+                'intervaloInicio' => 9800000,
+                'intervaloFim' => 10100000,
+                'total' => 35116
+            ],
+            [
+                'intervaloInicio' => 10100000,
+                'intervaloFim' => 10400000,
+                'total' => 36023
+            ],
+            [
+                'intervaloInicio' => 10400000,
+                'intervaloFim' => 10700000,
+                'total' => 36931
+            ],
+            [
+                'intervaloInicio' => 10700000,
+                'intervaloFim' => 11000000,
+                'total' => 37838
+            ],
+            [
+                'intervaloInicio' => 11000000,
+                'intervaloFim' => 11300000,
+                'total' => 38746
+            ],
+            [
+                'intervaloInicio' => 11300000,
+                'intervaloFim' => 11600000,
+                'total' => 39653
+            ],
+            [
+                'intervaloInicio' => 11600000,
+                'intervaloFim' => 11900000,
+                'total' => 40561
+            ],
+            [
+                'intervaloInicio' => 11900000,
+                'intervaloFim' => 12200000,
+                'total' => 41468
+            ],
+            [
+                'intervaloInicio' => 12200000,
+                'intervaloFim' => 12500000,
+                'total' => 42376
+            ],
+            [
+                'intervaloInicio' => 12500000,
+                'intervaloFim' => 12800000,
+                'total' => 43283
+            ],
+            [
+                'intervaloInicio' => 12800000,
+                'intervaloFim' => 13100000,
+                'total' => 44191
+            ],
+            [
+                'intervaloInicio' => 13100000,
+                'intervaloFim' => 13400000,
+                'total' => 45098
+            ],
+            [
+                'intervaloInicio' => 13400000,
+                'intervaloFim' => 13700000,
+                'total' => 46006
+            ],
+            [
+                'intervaloInicio' => 13700000,
+                'intervaloFim' => 14000000,
+                'total' => 46913
+            ],
+            [
+                'intervaloInicio' => 14000000,
+                'intervaloFim' => 14300000,
+                'total' => 47821
+            ],
+            [
+                'intervaloInicio' => 14300000,
+                'intervaloFim' => 14600000,
+                'total' => 48728
+            ],
+            [
+                'intervaloInicio' => 14600000,
+                'intervaloFim' => 14900000,
+                'total' => 49636
+            ],
+            [
+                'intervaloInicio' => 14900000,
+                'intervaloFim' => 15200000,
+                'total' => 50543
+            ],
+        );
+
+        foreach ($seeders as $seed) {
+            $this->db->table('fipe')->insert($seed);
+        }
+    }
+}
